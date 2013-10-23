84bec9f7c63522450c575d7cca4225af618bfcc5
Morgan Brown <brown.mhg@gmail.com>
2013-10-23 15:40:48 +0200

    Finish with the shapes

--- exercises/1/family.pl
+++ exercises/1/family.pl
@@ -13,6 +13,7 @@ mother(berta,guna). mother(berta,hugo). mother(dora,ina).   mother(dora,jane).
 mother(guna,mia).   mother(guna,olga).  mother(ina,wera).   mother(ina,rene).
 mother(ina,vico).   mother(jane,susan). mother(laura,tino). mother(laura,uta). 
 
+% 1.1
 person(F):- (mother(F,C1), mother(F, C2));
             (father(F,C1),father(F,C2)),
             male(C1),female(C2).
@@ -47,4 +48,38 @@ cousin(X,Y):- parent(PX,X),
                  (father(G,P); mother(G,P)),
                  (father(Q,C2); mother(Q,C2)), \+ P=Q,
                  (father(G,Q); mother(G,Q)). */
+
+% 1.2
+line(a, b). line(c, b). line(d, a).
+line(b, d). line(d, c). line(d, e).
+
+line1(X,Y) :- line(X,Y); line(Y,X).
+
+% a) triangle
+triangle(X,Y,Z) :-  line1(X, Y),line1(Y, Z),line1(Z, X).
+
+% b) quadrangle
+quadrangle(X,Y,Z,W) :-  line1(X, Y),line1(Y, Z),line1(Z, W), line1(W, X), \+ X=Z, \+ W=Y.
+% the inequalities prevent this from being a triangle.
+
+linep(point(1, 2), point(3, 4)).
+linep(point(2, 1), point(6, 0)).
+linep(point(3, 4), point(2, 1)).
+linep(point(6, 0), point(3, 4)).
+linep(point(2, 1), point(1, 2)).
+
+% c) Triangle with points. We define a new function that says that 
+% there is a line from one point to another in either direction
+line2(point(X1,Y1),point(X2,Y2)) :- linep(point(X1,Y1),point(X2,Y2)); linep(point(X2,Y2),point(X1,Y1)).
+
+trianglep(X1, Y1, X2, Y2, X3, Y3) :- line2(point(X1, Y1),point(X2, Y2)),
+                                     line2(point(X2, Y2),point(X3, Y3)),
+                                     line2(point(X3, Y3),point(X1, Y1)).
+
+% d) quadrangle with points
+quadranglep(X1, Y1, X2, Y2, X3, Y3, X4, Y4) :- line2(point(X1, Y1),point(X2, Y2)),
+                                             line2(point(X2, Y2),point(X3, Y3)),
+                                             line2(point(X3, Y3),point(X4, Y4)),
+                                             line2(point(X4, Y4),point(X1, Y1)),
+                                             \+ X1=X3, \+ Y1=Y3,\+ X2=X4, \+ Y2=Y4.
 
